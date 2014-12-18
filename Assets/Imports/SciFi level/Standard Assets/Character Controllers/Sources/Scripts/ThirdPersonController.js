56600f238b698a1f7bc7065c53fe44454f514eab
LLudoDev <ludovic.lucas.dev@gmail.com>
2014-12-18 17:31:15 +0100

    Map scene 1 v1

--- Assets/Imports/SciFi level/Standard Assets/Character Controllers/Sources/Scripts/ThirdPersonController.js
+++ Assets/Imports/SciFi level/Standard Assets/Character Controllers/Sources/Scripts/ThirdPersonController.js
@@ -1,6 +1,6 @@
 
 // Require a character controller to be attached to the same game object
-@script RequireComponent(Rigidbody)
+@script RequireComponent(CharacterController)
 
 public var idleAnimation : AnimationClip;
 public var walkAnimation : AnimationClip;
@@ -124,6 +124,108 @@ public var jumpPoseAnimation : AnimationClip;
 }
 
 
+function UpdateSmoothedMovementDirection ()
+{
+	var cameraTransform = Camera.main.transform;
+	var grounded = IsGrounded();
+	
+	// Forward vector relative to the camera along the x-z plane	
+	var forward = cameraTransform.TransformDirection(Vector3.forward);
+	forward.y = 0;
+	forward = forward.normalized;
+
+	// Right vector relative to the camera
+	// Always orthogonal to the forward vector
+	var right = Vector3(forward.z, 0, -forward.x);
+
+	var v = Input.GetAxisRaw("Vertical");
+	var h = Input.GetAxisRaw("Horizontal");
+
+	// Are we moving backwards or looking backwards
+	if (v < -0.2)
+		movingBack = true;
+	else
+		movingBack = false;
+	
+	var wasMoving = isMoving;
+	isMoving = Mathf.Abs (h) > 0.1 || Mathf.Abs (v) > 0.1;
+		
+	// Target direction relative to the camera
+	var targetDirection = h * right + v * forward;
+	
+	// Grounded controls
+	if (grounded)
+	{
+		// Lock camera for short period when transitioning moving & standing still
+		lockCameraTimer += Time.deltaTime;
+		if (isMoving != wasMoving)
+			lockCameraTimer = 0.0;
+
+		// We store speed and direction seperately,
+		// so that when the character stands still we still have a valid forward direction
+		// moveDirection is always normalized, and we only update it if there is user input.
+		if (targetDirection != Vector3.zero)
+		{
+			// If we are really slow, just snap to the target direction
+			if (moveSpeed < walkSpeed * 0.9 && grounded)
+			{
+				moveDirection = targetDirection.normalized;
+			}
+			// Otherwise smoothly turn towards it
+			else
+			{
+				moveDirection = Vector3.RotateTowards(moveDirection, targetDirection, rotateSpeed * Mathf.Deg2Rad * Time.deltaTime, 1000);
+				
+				moveDirection = moveDirection.normalized;
+			}
+		}
+		
+		// Smooth the speed based on the current target direction
+		var curSmooth = speedSmoothing * Time.deltaTime;
+		
+		// Choose target speed
+		//* We want to support analog input but make sure you cant walk faster diagonally than just forward or sideways
+		var targetSpeed = Mathf.Min(targetDirection.magnitude, 1.0);
+	
+		_characterState = CharacterState.Idle;
+		
+		// Pick speed modifier
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))
+		{
+			targetSpeed *= runSpeed;
+			_characterState = CharacterState.Running;
+		}
+		else if (Time.time - trotAfterSeconds > walkTimeStart)
+		{
+			targetSpeed *= trotSpeed;
+			_characterState = CharacterState.Trotting;
+		}
+		else
+		{
+			targetSpeed *= walkSpeed;
+			_characterState = CharacterState.Walking;
+		}
+		
+		moveSpeed = Mathf.Lerp(moveSpeed, targetSpeed, curSmooth);
+		
+		// Reset walk time start when we slow down
+		if (moveSpeed < walkSpeed * 0.3)
+			walkTimeStart = Time.time;
+	}
+	// In air controls
+	else
+	{
+		// Lock camera while in air
+		if (jumping)
+			lockCameraTimer = 0.0;
+
+		if (isMoving)
+			inAirVelocity += targetDirection.normalized * Time.deltaTime * inAirControlAcceleration;
+	}
+	
+
+		
+}
 
 
 function ApplyJumping ()
@@ -185,35 +287,35 @@ function DidJump ()
 }
 
 function Update() {
-
-	var rigid : Rigidbody = GetComponent(Rigidbody);
-    // Pick speed modifier
-	if (rigid.velocity.magnitude > 2)
+	
+	if (!isControllable)
 	{
-	    _characterState = CharacterState.Running;
-	}
-	else if (rigid.velocity.magnitude > 0)
-	{
-	    _characterState = CharacterState.Trotting;
-	}
-	else
-	{
-	    _characterState = CharacterState.Walking;
+		// kill all inputs if not controllable.
+		Input.ResetInputAxes();
 	}
 
+	if (Input.GetButtonDown ("Jump"))
+	{
+		lastJumpButtonTime = Time.time;
+	}
+
+	UpdateSmoothedMovementDirection();
 	
 	// Apply gravity
 	// - extra power jump modifies gravity
 	// - controlledDescent mode modifies gravity
+	ApplyGravity ();
 
 	// Apply jumping logic
+	ApplyJumping ();
 	
 	// Calculate actual motion
 	var movement = moveDirection * moveSpeed + Vector3 (0, verticalSpeed, 0) + inAirVelocity;
 	movement *= Time.deltaTime;
 	
 	// Move the controller
-	var controller : Rigidbody = GetComponent(Rigidbody);
+	var controller : CharacterController = GetComponent(CharacterController);
+	collisionFlags = controller.Move(movement);
 	
 	// ANIMATION sector
 	if(_animation) {
@@ -231,23 +333,21 @@ function Update() {
 		} 
 		else 
 		{
-			if(controller.velocity.sqrMagnitude <= 0.0) {
+			if(controller.velocity.sqrMagnitude < 0.1) {
 				_animation.CrossFade(idleAnimation.name);
 			}
 			else 
 			{
-
-			    if(_characterState == CharacterState.Running) {
-
+				if(_characterState == CharacterState.Running) {
 					_animation[runAnimation.name].speed = Mathf.Clamp(controller.velocity.magnitude, 0.0, runMaxAnimationSpeed);
 					_animation.CrossFade(runAnimation.name);	
 				}
 				else if(_characterState == CharacterState.Trotting) {
-					_animation[walkAnimation.name].speed = Mathf.Clamp(controller.velocity.magnitude, 0.0, 0.9f);
+					_animation[walkAnimation.name].speed = Mathf.Clamp(controller.velocity.magnitude, 0.0, trotMaxAnimationSpeed);
 					_animation.CrossFade(walkAnimation.name);	
 				}
 				else if(_characterState == CharacterState.Walking) {
-					_animation[walkAnimation.name].speed = Mathf.Clamp(controller.velocity.magnitude, 0.0, 0.9f);
+					_animation[walkAnimation.name].speed = Mathf.Clamp(controller.velocity.magnitude, 0.0, walkMaxAnimationSpeed);
 					_animation.CrossFade(walkAnimation.name);	
 				}
 				
