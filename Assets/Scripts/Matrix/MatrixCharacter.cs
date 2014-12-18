3c2a8e2e5a5f4b0544654fc9d540447743bf3fc4
LLudoDev <ludovic.lucas.dev@gmail.com>
2014-12-18 23:03:47 +0100

    Matrix

---
+++ Assets/Scripts/Matrix/MatrixCharacter.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatrixCharacter : MonoBehaviour {
+
+    private int nbCharacters = 8;
+    private static char[] characters = { '嫃', '媽', '唷', '啤', '啥', '営', '唸', '嗇' };
+
+    public float lifeTime = 3;
+    public float vitesseAffichage = .5f;
+    private bool nextCreated = false;
+    private float time = 0;
+
+    public float espacement = 1;
+    public GameObject prefabMatrixCharacter;
+
+    private float timeForSwitching;
+    private float switchingTime = 0;
+
+    public int nbFils = 3;
+
+	void Start () {
+        if (vitesseAffichage > lifeTime)
+            Debug.LogError("Le MatrixCharacter ne vit pas assez longtemps pour créer son suivant.");
+        GetComponent<TextMesh>().text = "" + characters[Random.Range(0, nbCharacters)];
+        timeForSwitching = Random.Range(.5f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time += Time.deltaTime;
+        switchingTime += Time.deltaTime;
+
+        if(switchingTime >= timeForSwitching )
+        {
+            switchingTime -= timeForSwitching;
+            GetComponent<TextMesh>().text = "" + characters[Random.Range(0, nbCharacters)];
+        }
+
+        if (time >= lifeTime)
+            GameObject.Destroy(gameObject);
+
+        if (nbFils > 0 && !nextCreated && time >= vitesseAffichage)
+        {
+            GameObject newObj = (GameObject)GameObject.Instantiate(prefabMatrixCharacter);
+            newObj.transform.parent = transform.parent;
+            newObj.GetComponent<MatrixCharacter>().nbFils = nbFils - 1;
+            Vector2 position = transform.position;
+            position.y -= espacement;
+            newObj.transform.position = position;
+            nextCreated = true;
+        }
+
+        if (time <= vitesseAffichage)
+        {
+            GetComponent<TextMesh>().color = new Color(1, 1, 1);
+        }
+        else
+        {
+            GetComponent<TextMesh>().color = new Color(28f / 255, 236f / 255, 21f / 255, 1 - (time - vitesseAffichage) / (lifeTime - vitesseAffichage));
+        }
+	}
+}
---
+++ Assets/Scripts/Matrix/MatrixGenerator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatrixGenerator : MonoBehaviour {
+
+    public float sizeX = 1;
+    public float sizeY = 1;
+    public float timeMinBeforeGeneration = 0;
+    public float timeMaxBeforeGeneration = 1;
+    public float nbNewCharacterPerTick = 3;
+    public int minRepetitionMatrixCharacter = 3;
+    public int maxRepetitionMatrixCharacter = 15;
+    public GameObject prefabMatrixCharacter;
+    private float espacement;
+
+    private float time = 0;
+
+	void Start () {
+
+        if (prefabMatrixCharacter == null)
+            Debug.LogError("Aucun prefab de colonne de matrice linké");
+
+        espacement = prefabMatrixCharacter.GetComponent<MatrixCharacter>().espacement;
+        time = Random.Range(timeMinBeforeGeneration, timeMaxBeforeGeneration);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (prefabMatrixCharacter == null)
+            return;
+
+        time -= Time.deltaTime;
+
+        if(time <= 0)
+        {
+            for (int i = 0; i < nbNewCharacterPerTick;++i )
+            {
+                time = -time + Random.Range(timeMinBeforeGeneration * 100, timeMaxBeforeGeneration * 100) / 100f;
+                float futureX = Random.Range(0, sizeX) - sizeX / 2;
+                futureX -= futureX % espacement;
+                float futureY = Random.Range(0, sizeY) - sizeY / 2;
+                futureY -= futureY % espacement;
+                GameObject newMatrixCharacter = (GameObject)GameObject.Instantiate(prefabMatrixCharacter);
+                newMatrixCharacter.transform.parent = transform;
+                newMatrixCharacter.transform.position = new Vector2(futureX, futureY) + (Vector2)transform.position;
+                newMatrixCharacter.GetComponent<MatrixCharacter>().nbFils = Random.Range(minRepetitionMatrixCharacter, maxRepetitionMatrixCharacter);
+            }
+        }
+	}
+}
