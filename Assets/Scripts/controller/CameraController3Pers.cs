cfd1d918dd020822bbc9fe15c70a1d9c7bdf2ad5
Mickael Houg <hougmickael@gmail.com>
2014-12-18 20:24:17 +0100

    Controller3pers + Camera3pers

--- Assets/Scripts/controller/CameraController3Pers.cs
+++ Assets/Scripts/controller/CameraController3Pers.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class CameraController3Pers : MonoBehaviour
 {
     public Transform target;
+    public Transform targetModelBody;
+    public Transform bodyPivot;
     public float optimalRange = 10;
-    public float localAngleX;
     public float localAngleY;
     public float speedRotateX = 50;
     public float speedRotateY = 50;
@@ -13,42 +14,56 @@ public class CameraController3Pers : MonoBehaviour
 
     public bool blockMouse = false;
 
+
     void Start()
     {
+        
     }
-
-    void LateUpdate()
+    void Update()
     {
-        Vector2 joystickRight = new Vector2(Input.GetAxis("JoyRightHorizontal"),Input.GetAxis("JoyRightVertical"));
+        Vector2 joystickRight = new Vector2(Input.GetAxis("JoyRightHorizontal"), Input.GetAxis("JoyRightVertical"));
         Vector2 mouseXY = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         Vector2 result = (joystickRight + mouseXY).normalized;
 
-        localAngleY -= result.y * Time.deltaTime * speedRotateY;
-        localAngleX += result.x * Time.deltaTime * speedRotateX;
+        if (!blockMouse)
+        {
+            Screen.lockCursor = true;
+            localAngleY += result.y * Time.deltaTime * speedRotateY;
+       localAngleY = Mathf.Clamp(localAngleY, -60, 50);
+        target.Rotate(Vector3.up, result.x * Time.deltaTime * speedRotateX, Space.Self);
+        }
+        else
+        {
+        
+        }
+    }
+    void LateUpdate()
+    {
+        targetModelBody.Rotate(Vector3.forward, localAngleY, Space.Self);
 
-        target.transform.Rotate(Vector3.up, result.y * Time.deltaTime * speedRotateY,Space.Self);
-        target.transform.Rotate(Vector3.right, result.x * Time.deltaTime * speedRotateX, Space.Self);
+        Vector3 direction = target.forward;
 
-       // Vector3 charDirection = Quaternion.AngleAxis(localAngleY, Vector3.right) * target.transform.forward;
-        //charDirection = Quaternion.AngleAxis(localAngleX, Vector3.up) * charDirection;
-
-        Vector3 direction = target.transform.forward;
-       
         transform.LookAt(transform.position + direction);
-        Ray ray = new Ray(target.transform.position + offset, -direction);
+        bodyPivot.LookAt(bodyPivot.position + direction);
+        bodyPivot.Rotate(Vector3.right, -localAngleY, Space.Self);
+
+        transform.LookAt(transform.position + bodyPivot.forward);
+        Ray ray = new Ray(bodyPivot.position + (bodyPivot.TransformPoint(offset) - bodyPivot.position), -direction);
         RaycastHit[] hits = Physics.RaycastAll(ray, optimalRange);
         bool hitSomething = false;
         if (hits.Length > 0)
         {
             foreach (RaycastHit hit in hits)
             {
-                if (!hitSomething || (transform.position - ray.origin).magnitude > (hit.point - ray.origin).magnitude)
+                if (hit.collider.gameObject.tag != "Player" && !hit.collider.isTrigger && (!hitSomething || (transform.position - ray.origin).magnitude > (hit.point - ray.origin).magnitude) )
                 {
+                    Debug.Log(hit.collider.isTrigger);
                     transform.position = hit.point;
-                    transform.Translate( hit.normal * 0.1f);
+                    transform.Translate(hit.normal * 0.1f);
+                    hitSomething = true;
+
                 }
-                hitSomething = true;
             }
         }
         if (!hitSomething)
