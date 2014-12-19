e153ce75fbf83a7e60d76e373d2633618b7bc2df
LLudoDev <ludovic.lucas.dev@gmail.com>
2014-12-19 02:47:51 +0100

    MiniJeu v2

---
+++ Assets/Scripts/BarrelController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelController : MonoBehaviour {
+
+    private bool blocked = false;
+    private bool willReload = false;
+    float timeBeforeReloading = 4;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(willReload)
+        {
+            timeBeforeReloading -= Time.deltaTime;
+            if (timeBeforeReloading <= 0)
+            {
+                Application.LoadLevel(4);
+                willReload = false;
+            }
+        }
+
+        if (blocked)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            return;
+        }
+
+        GetComponent<Rigidbody>().velocity = SpaceNavigatorNoDevice.SubInstance.GetTranslation();
+	}
+
+    void OnCollisionEnter(Collision collider)
+    {
+        GameManager.getInstance().setGameLost("Ne touchez aucun obstacle avec le produit!!");
+        willReload = true;
+        blocked = true;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Generator")
+        {
+            GameManager.getInstance().setGameWin();
+            blocked = true;
+        }
+    }
+}
